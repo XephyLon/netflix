c5dfd36c1f14510bf8f2257924ac7101e8257876
Xephy Lon <adr3nalinexin@gmail.com>
2024-08-13 11:10:56 +0300

    refactor: mapped movies observable to immediately fetch the videos

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -6,39 +6,41 @@ import { BannerComponent } from '../../core/components/banner/banner.component';
 import { MovieService } from '../../shared/services/movie.service';
 import { MovieCarouselComponent } from '../../shared/components/movie-carousel/movie-carousel.component';
 import { map, Observable, take } from 'rxjs';
-import { movieData } from '../../shared/models/video';
+import { movieData, Video } from '../../shared/models/video';
 
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [HeaderComponent , BannerComponent , MovieCarouselComponent ,AsyncPipe , JsonPipe],
+  imports: [HeaderComponent, BannerComponent, MovieCarouselComponent, AsyncPipe, JsonPipe],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss'
 })
 export class BrowseComponent {
   auth = inject(AuthService)
-  userData:any;
-movieService = inject(MovieService)
-movies$:Observable<movieData> = this.movieService.getMovies()
-data!:any
+  userData: any;
+  movieService = inject(MovieService)
+  movies$: Observable<Video[]> = this.movieService.getMovies().pipe(
+    map(data => data.results)
+  )
+  data!: any
 
-ngOnInit(): void {
- this.movieService.getMovies().subscribe(res=>
-  console.log(res)
- )
-}
-  
-  platformid!:Object
-  constructor(@Inject(PLATFORM_ID) platfromid:Object){
+  ngOnInit(): void {
+    this.movieService.getMovies().subscribe(res =>
+      console.log(res)
+    )
+  }
+
+  platformid!: Object
+  constructor(@Inject(PLATFORM_ID) platfromid: Object) {
     this.platformid = platfromid
-      if (isPlatformBrowser(this.platformid)) {
-    this.userData = JSON.parse(sessionStorage.getItem('loginuser')!)
-    
-  }
+    if (isPlatformBrowser(this.platformid)) {
+      this.userData = JSON.parse(sessionStorage.getItem('loginuser')!)
+
+    }
   }
 
-  singOut(){
+  singOut() {
     this.auth.singOut()
   }
 
